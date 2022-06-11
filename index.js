0a4a1f8eeaf3b8b51357405ccddce3bfeedfa5e2
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-11 19:05:05 +0900

    fork

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://humanscape-team5a.netlify.app"],
+};
+
+app.use((_, res) => {
+  res.header("Access-Control-Allow-Origin", "https://humanscape-team5a.netlify.app");
+});
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  const textQuery = req.query.searchText;
+  const numOfRowsQuery = req.query.numOfRows;
+
+  axios
+    .get(
+      "http://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList",
+      {
+        params: {
+          sickType: 1,
+          medTp: 2,
+          diseaseType: "SICK_NM",
+          searchText: textQuery,
+          ServiceKey: process.env.KEY,
+          numOfRows: numOfRowsQuery,
+          _type: "json",
+        },
+      }
+    )
+    .then(response => res.send(response.data));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
