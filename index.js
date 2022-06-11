3c75f9c3d9fe78c230bcaa73795fbe7f164cc1aa
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-11 20:23:26 +0900

    test

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
-require("dotenv").config();
+const config = require("dotenv").config();
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://humanscape-team5a.netlify.app"],
@@ -14,33 +12,36 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
-app.use((_, res) => {
-  res.header("Access-Control-Allow-Origin", "https://humanscape-team5a.netlify.app");
-});
-
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  // const textQuery = req.query.searchText;
-  // const numOfRowsQuery = req.query.numOfRows;
-
-  axios
-    .get(
-      "http://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList",
-      {
-        params: {
-          sickType: 1,
-          medTp: 2,
-          // searchText: textQuery,
-          ServiceKey: 'uVvMtekN5Q4wkwgFzjYoou6YPy56Ox0s0YIRX7gYF0RxwLLX7vdQCRjUrHaDArbnrP9vyy/IdHBVKI2exzPF9w==',
-          // numOfRows: numOfRowsQuery,
-          _type: "json",
-        },
-      }
-    )
-    .then(response => res.send(response.data));
+app.use((_, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://humanscape-team5a.netlify.app",
+  );
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.get("/", async (req, res) => {
+  const { searchText, numOfRows } = req.query;
+
+  const response = await axios.get(
+    "http://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList",
+    {
+      params: {
+        ServiceKey: config.parsed.KEY,
+        _type: "json",
+        sickType: 1,
+        medTp: 2,
+        searchText,
+        numOfRows,
+      },
+    },
+  );
+
+  console.log(response);
+  res.send(response.data);
+});
+
+app.listen(8080, () => {
+  console.log(`Server running on ${8080}`);
 });
