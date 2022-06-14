ddd7080fcbb6b39f917600ffffe474375f64a049
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-15 02:42:11 +0900

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-import axios from 'axios';
+import axios from "axios";
+import cors from "cors";
 import type { Request, Response, NextFunction } from "express";
 
 const PORT = config().parsed?.port || 8080;
@@ -8,12 +9,20 @@ const KEY = config().parsed?.KEY;
 
 const app = express();
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000", "http://localhost:3001");
+});
+
 app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   // const data = await readFilePromise('./data/dissNameCodeList.json');
   // console.log(data);
-  console.log('request in');
-  const r = await axios.get(`http://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList?ServiceKey=${KEY}&numOfRows=2000&diseaseType=SICK_NM&medTp=2&_type=json`);
-  console.log(r.data)
+  console.log("request in");
+  const r = await axios.get(
+    `http://apis.data.go.kr/B551182/diseaseInfoService/getDissNameCodeList?ServiceKey=${KEY}&numOfRows=2000&diseaseType=SICK_NM&medTp=2&_type=json`,
+  );
+  console.log(r.data);
   res.send(r.data.response.body);
 });
 
