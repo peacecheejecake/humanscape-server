1b1efa36e12386bd6f22e6363a599e446fccabe9
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-15 03:54:12 +0900

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://humanscape-client.vercel.app/', 'http://localhost:3000'],
+    origin: 'https://humanscape-client.vercel.app',
   }),
 );
 
