67b2b652256c023d5f387d7a23fdf57835cbf233
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-15 09:33:01 +0900

    test

--- src/app.ts
+++ src/app.ts
@@ -16,9 +16,9 @@ const app = express();
 // );
 app.use(cors());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+});
 
 app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   // const data = await readFilePromise('./data/dissNameCodeList.json');
